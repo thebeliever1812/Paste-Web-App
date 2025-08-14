79b5359bc0bab5d107b4b0f26596aa0d0488b20b
Basir Ahmad <basirahmadmalik@gmail.com>
2025-08-14 15:22:26 +0530

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: process.env.VITE_BASE_PATH || "/Paste-Web-App",
+	base: process.env.VITE_BASE_PATH || "/",
 });
